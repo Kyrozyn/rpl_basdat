577399bd83753b1bacf925a4627f02e1b3643f67
syahrilasidiq <41316515+syahrilasidiq@users.noreply.github.com>
2018-07-17 15:42:57 +0700

    memeg
    edit id ke name

--- form_tambahkendaraan.php
+++ form_tambahkendaraan.php
@@ -19,19 +19,19 @@ cekRole("pemilik");
         <form action="/action_page.php">
             <div class="form-group">
                 <label for="No_Pol">Nomor Polisi:</label>
-                <input type="text" class="form-control" id="No_Pol">
+                <input type="text" class="form-control" name="No_Pol">
             </div>
             <div class="form-group">
                 <label for="Merk">Merk Mobil</label>
-                <input type="text" class="form-control" id="Merk">
+                <input type="text" class="form-control" name="Merk">
             </div>
             <div class="form-group">
                 <label for="Tahun_Buat">Tahun Buat</label>
-                <input type="text" class="form-control" id="Tahun_Buat">
+                <input type="text" class="form-control" name="Tahun_Buat">
             </div>
             <div class="form-group">
                 <label for="Bahan_Bakar">Bahan Bakar</label>
-                <select class="form-control" id="Bahan_Bakar">
+                <select class="form-control" name="Bahan_Bakar">
                     <option value="Pertalite">Pertalite</option>
                     <option value="Premium">Premium</option>
                     <option value="Pertamax">Pertamax</option>
@@ -39,15 +39,15 @@ cekRole("pemilik");
             </div>
             <div class="form-group">
                 <label for="Warna">Warna</label>
-                <input type="text" class="form-control" id="Warna">
+                <input type="text" class="form-control" name="Warna">
             </div>
             <div class="form-group">
                 <label for="Isi_Silinder">Isi Silinder</label>
-                <input type="text" class="form-control" id="Isi_Silinder">
+                <input type="text" class="form-control" name="Isi_Silinder">
             </div>
             <div class="form-group">
                 <label for="Harga_Sewa">Harga Sewa</label>
-                <input type="text" class="form-control" id="Harga_Sewa">
+                <input type="text" class="form-control" name="Harga_Sewa">
             </div>
             <button type="reset" class="btn btn-primary">Reset</button>
             <button type="submit" class="btn btn-primary">Submit</button>
