81d42f6f52299c75e73effbea975935dd2ad4828
syahrilasidiq <41316515+syahrilasidiq@users.noreply.github.com>
2018-07-17 15:34:37 +0700

    add form tambahkendaraan
    form_tambahkendaraan.php file in rpl_basdat

---
+++ form_tambahkendaraan.php
@@ -0,0 +1,62 @@
+<?php
+include "setting/head.php";
+include "setting/cekredirect.php";
+echo "role = " . $_SESSION['role'] . "<br>";
+cekRole("pemilik");
+?>
+
+<body>
+<!-- Atas !-->
+<?php include "setting/atas.php" ?>
+<!--!-->
+<div class="text-center jumbotron">
+    <!-- Kiri !-->
+    <?php include "setting/kiri.php" ?>
+    <!--!-->
+    <!-- Tengah !-->
+    <div class="col-sm-10 text-left">
+        <h1 class="text-center">Tambah Data</h1>
+        <form action="/action_page.php">
+            <div class="form-group">
+                <label for="No_Pol">Nomor Polisi:</label>
+                <input type="text" class="form-control" id="No_Pol">
+            </div>
+            <div class="form-group">
+                <label for="Merk">Merk Mobil</label>
+                <input type="text" class="form-control" id="Merk">
+            </div>
+            <div class="form-group">
+                <label for="Tahun_Buat">Tahun Buat</label>
+                <input type="text" class="form-control" id="Tahun_Buat">
+            </div>
+            <div class="form-group">
+                <label for="Bahan_Bakar">Bahan Bakar</label>
+                <select class="form-control" id="Bahan_Bakar">
+                    <option value="Pertalite">Pertalite</option>
+                    <option value="Premium">Premium</option>
+                    <option value="Pertamax">Pertamax</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="Warna">Warna</label>
+                <input type="text" class="form-control" id="Warna">
+            </div>
+            <div class="form-group">
+                <label for="Isi_Silinder">Isi Silinder</label>
+                <input type="text" class="form-control" id="Isi_Silinder">
+            </div>
+            <div class="form-group">
+                <label for="Harga_Sewa">Harga Sewa</label>
+                <input type="text" class="form-control" id="Harga_Sewa">
+            </div>
+            <button type="reset" class="btn btn-primary">Reset</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
+    <!--end tengah!-->
+</div>
+<!-- Footer -->
+
+<!-- Footer -->
+</body>
