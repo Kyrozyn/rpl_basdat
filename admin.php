ab1a62b49b22c1782d099c74eaf7f39ccf07a5b9
andreas <arizkirizaldi@email.unikom.ac.id>
2018-07-17 12:46:34 +0700

    done edit kiri + pisah

--- admin.php
+++ admin.php
@@ -1,40 +1,29 @@
 <?php
 include "setting/head.php";
 include "setting/cekredirect.php";
-echo "role = ".$_SESSION['role']."<br>";
+echo "role = " . $_SESSION['role'] . "<br>";
 cekRole("pemilik");
 ?>
 <body>
-<nav class="navbar navbar-inverse navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <div class="container-fluid">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">Bastian Rental Mobil</a>
-    <ul class="navbar-nav px-4">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href="logout.php">Sign out</a>
-        </li>
-    </ul>
+<!-- Atas !-->
+<?php include "setting/atas.php" ?>
+<!--!-->
+<div class="text-center jumbotron">
+    <!-- Kiri !-->
+    <?php include "setting/kiri.php" ?>
+    <!--!-->
+    <!-- Tengah !-->
+    <div class="col-sm-10 text-left">
+        <h1>Welcome to kontoru site</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.</p>
+        <hr>
+        <h3>Test</h3>
     </div>
-</nav>
-
-<!-- Sesudah nav atas !-->
-<div class="container text-center jumbotron">
-    <div class="row content">
-        <!-- Kiri !-->
-        <div class="col-sm-2 sidenav">
-            <p><a href="#">Kontoru</a></p>
-            <p><a href="#">Test</a></p>
-            <p><a href="#">Tilu</a></p>
-        </div>
-        <!-- Tengah !-->
-        <div class="col-sm-10 text-left">
-            <h1>Welcome to kontoru site</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <hr>
-            <h3>Test</h3>
-            <p>Lorem ipsum...</p>
-        </div>
-
-    </div>
+    <!--end tengah!-->
 </div>
-
 </body>
---
+++ setting/kiri.php
@@ -0,0 +1,33 @@
+
+    <div class="row content">
+        <nav class="col-md-2 d-none d-md-block sidebar">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <!--Item!-->
+                    <h6 class="sidebar-heading text-muted">
+                        <span>Pengolahan Sewa</span>
+                    </h6>
+                    <div class="list-group text-left">
+                        <a class="list-group-item" href="#"><i class="fa fa-car fa-fw" aria-hidden="true"></i>&nbsp; Data Kendaraan</a>
+                        <a class="list-group-item" href="#"><i class="fa fa-id-card-o fa-fw" aria-hidden="true"></i>&nbsp; Data Driver</a>
+                        <a class="list-group-item" href="#"><i class="fa fa-user-circle-o fa-fw" aria-hidden="true"></i>&nbsp; Data Customer</a>
+                        <a class="list-group-item" href="#"><i class="fa fa-check fa-fw" aria-hidden="true"></i>&nbsp; Data Sewa</a>
+                    </div>
+                    <!--!-->
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Pengolahan Pegawai</span>
+                    </h6>
+                    <div class="list-group text-left">
+                        <a class="list-group-item" href="#"><i class="fa fa-child fa-fw" aria-hidden="true"></i>&nbsp; Data Pegawai</a>
+                    </div>
+                    <!--!-->
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Laporan</span>
+                    </h6>
+                    <div class="list-group text-left">
+                        <a class="list-group-item" href="#"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Laporan</a>
+                    </div>
+                    <!--end item!-->
+                </ul>
+            </div>
+        </nav>
