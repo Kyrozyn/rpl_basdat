ba7e8a0120326648ad4e3717c38f0c0482d1e278
andreas <arizkirizaldi@email.unikom.ac.id>
2018-07-17 11:29:27 +0700

    cek2

--- admin.php
+++ admin.php
@@ -17,14 +17,16 @@ cekRole("pemilik");
     </div>
 </nav>
 
-<!-- MAIN !-->
+<!-- Sesudah nav atas !-->
 <div class="container text-center jumbotron">
     <div class="row content">
+        <!-- Kiri !-->
         <div class="col-sm-2 sidenav">
             <p><a href="#">Link</a></p>
             <p><a href="#">Link</a></p>
             <p><a href="#">Link</a></p>
         </div>
+        <!-- Tengah !-->
         <div class="col-sm-10 text-left">
             <h1>Welcome</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
